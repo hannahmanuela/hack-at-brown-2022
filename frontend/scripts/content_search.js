a717085962d0501cc493cdcbd056e88a2cc71e4d
Megan Frisella <meganfrisella@gmail.com>
2022-01-29 16:52:28 -0500

    add new search link

--- frontend/scripts/content_search.js
+++ frontend/scripts/content_search.js
@@ -15,5 +15,14 @@ if (window.location.hostname == "www.google.com"){
             document.getElementById('socRisk').innerHTML = response.esg.social;
             document.getElementById('govRisk').innerHTML = response.esg.governance;
             document.getElementById('cont').innerHTML = response.esg.controversy;
+
+            const tok_name = response.name.split(" ");
+            search_url = "https://www.google.com/search?q=";
+            for (let i = 0; i < length(tok_name); i++) {
+                search_url.concat(tok_name[i] + "+")
+            }
+            search_url = search_url + "sustainability";
+            console.log(search_url);
+            document.getElementById('newSearch').setAttribute('href', search_url);
     } )
 }
