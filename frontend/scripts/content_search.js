c3d791aaba69c6211f214c67ea2ff3e4124ffad9
Megan Frisella <meganfrisella@gmail.com>
2022-01-29 16:20:11 -0500

    resolve doman issues

--- frontend/scripts/content_search.js
+++ frontend/scripts/content_search.js
@@ -2,14 +2,14 @@
 if (window.location.hostname == "www.google.com"){
     const urlParams = new URLSearchParams(window.location.search);
     const query = urlParams.get('q');
-    alert("query: " + query);
-
+    console.log(domain);
     
     // send query to backend
-    fetch('{.env.backend_url}/{query}')
+    fetch('https://esgify.herokuapp.com/esg/' + query)
         .then( response => response.json() )
         .then( response => {
             // todo this??
+            console.log("log2");
             document.getElementById('ticker').innerHTML = response.ticker;
             document.getElementById('envRisk').innerHTML = response.esg.environment;
             document.getElementById('socRisk').innerHTML = response.esg.social;
